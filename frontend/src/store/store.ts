da3cfe06eab3d5e5cb3e882158922bfe6fabe267
JanofarJahan <janofarjahan2000@gmail.com>
2024-11-19 00:37:09 +0530

    env and store changes

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,14 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import chatReducer from './reducers/chatSlice';
-import userReducer from './reducers/userSlice';
-import messageReducer from './reducers/messageSlice';
 import authReducer from './reducers/authSlice';
 
 export const store = configureStore({
   reducer: {
     chat: chatReducer,
-    user : userReducer,
-    message : messageReducer,
     auth : authReducer,
   },
 });
